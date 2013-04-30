d4eac275913235568c6e7ecf5c58ff97dcdd15af
jvbruggen <j@stimulusresponse.org>
2013-04-30 16:22:56 +0100

    compare to font-size instead of background color

--- src/js/main.js
+++ src/js/main.js
@@ -21,19 +21,11 @@ $(document).ready(function() {
 
 	$(window).resize(function() {
 
-<<<<<<< HEAD
 		if(parseInt($('#brand-logo').css('font-size')) === 0 && $('#prime-nav').css('display') != 'block') {
 			$('#prime-nav').toggle();
 		}
 
 		if(parseInt($('#brand-logo').css('font-size')) !== 0 && $('#prime-nav').css('display') == 'block') {
-=======
-		if(parseInt($('#brand-logo').css('font-size')) == 0 && $('#prime-nav').css('display') != 'block') {
-			$('#prime-nav').toggle();
-		}
-
-		if(parseInt($('#brand-logo').css('font-size')) != 0 && $('#prime-nav').css('display') == 'block') {
->>>>>>> changed comparison to font-size
 			$('#prime-nav').toggle();
 			$('#site-head .toggle-menu').removeClass('active');
 		}
