c1649feff09856225d6b0d38856ffc91fe58f2ac
jvbruggen <j@stimulusresponse.org>
2013-05-02 16:37:41 +0100

    permissions change

--- public/postreceive.php
+++ public/postreceive.php
@@ -31,7 +31,7 @@ if ( $payload->ref === 'refs/heads/master' && $_REQUEST['key'] == SERVER_KEY ) {
 		/* define the cd directory based on config
 		cd into the project dir, git reset and pull changes */
 		$output = (shell_exec( 'cd ' . PROJECTS_PATH . '/ && git reset --hard HEAD && git pull' ));
-		error_log($output);
+		error_log($output); 
 }
 
 ?>
