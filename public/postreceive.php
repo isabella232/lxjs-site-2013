3bea079e61e0e6a832c2a1d90bd24f5d9c49edae
jvbruggen <j@stimulusresponse.org>
2013-05-02 11:50:50 +0100

    remove project name from path

--- public/postreceive.php
+++ public/postreceive.php
@@ -29,7 +29,7 @@ if ( $payload->ref === 'refs/heads/master' && $_REQUEST['key'] == SERVER_KEY ) {
         // parse the payload for the project name
         $project_name = strtolower($payload->{'repository'}->{'name'});
         // define the cd directory based on config and project name
-        $project_directory = PROJECTS_PATH . $project_name;
+        $project_directory = PROJECTS_PATH ;
 
         // cd into the project dir, git reset and pull changes
         shell_exec( 'cd ' . $project_directory . '/ && git reset --hard HEAD && git pull' );
