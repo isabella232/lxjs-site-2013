298c44de17060a212bef36ed32f6d0fea793d2a5
Leo Xavier <leonardo.xavier@gmail.com>
2013-06-14 14:29:54 +0100

    Added Mark Boas

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,8 @@ module.exports = function(grunt) {
         "public/speakers/robert-nyman.html": "src/speakers/robert-nyman.html",
         "public/speakers/bill-mills.html": "src/speakers/bill-mills.html",
         "public/speakers/angelina-fabbro.html": "src/speakers/angelina-fabbro.html",
-        "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html"
+        "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html",
+        "public/speakers/mark-boas.html": "src/speakers/mark-boas.html"
   			}
   		},
 
@@ -104,7 +105,8 @@ module.exports = function(grunt) {
         "public/speakers/robert-nyman.html": "src/speakers/robert-nyman.html",
         "public/speakers/bill-mills.html": "src/speakers/bill-mills.html",
         "public/speakers/angelina-fabbro.html": "src/speakers/angelina-fabbro.html",
-        "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html"
+        "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html",
+        "public/speakers/mark-boas.html": "src/speakers/mark-boas.html"
 			}
   		}
   	},
