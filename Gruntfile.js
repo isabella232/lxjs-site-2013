cbc3d7357282a13348e768efef6ffdde53aa5ae3
jvbruggen <j@stimulusresponse.org>
2013-08-08 11:19:05 +0100

    reindenting to test the server pull

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,49 +50,49 @@ module.exports = function(grunt) {
   				"public/team-and-contacts.html": "src/team-and-contacts.html",
   				"public/workshops.html": "src/workshops.html",
   				"public/speakers/index.html": "src/speakers/index.html",
-				"public/speakers/mike-brevoort.html": "src/speakers/mike-brevoort.html",
-        "public/speakers/adam-baldwin.html": "src/speakers/adam-baldwin.html",
-        "public/speakers/jonathan-lipps.html": "src/speakers/jonathan-lipps.html",
-				"public/speakers/sara-chipps.html": "src/speakers/sara-chipps.html",
-				"public/speakers/vyacheslav-egorov.html": "src/speakers/vyacheslav-egorov.html",
-				"public/speakers/dustin-diaz.html": "src/speakers/dustin-diaz.html",
-        "public/speakers/brian-leroux.html": "src/speakers/brian-leroux.html",
-        "public/speakers/laura-kalbag.html": "src/speakers/laura-kalbag.html",
-        "public/speakers/shane-hudson.html": "src/speakers/shane-hudson.html",
-        "public/speakers/jed-schmidt.html": "src/speakers/jed-schmidt.html",
-        "public/speakers/andrew-nesbitt.html": "src/speakers/andrew-nesbitt.html",
-        "public/speakers/gilles-ruppert.html": "src/speakers/gilles-ruppert.html",
-        "public/speakers/raquel-velez.html": "src/speakers/raquel-velez.html",
-        "public/speakers/ivan-babrou.html": "src/speakers/ivan-babrou.html",
-        "public/speakers/robert-nyman.html": "src/speakers/robert-nyman.html",
-        "public/speakers/bill-mills.html": "src/speakers/bill-mills.html",
-        "public/speakers/angelina-fabbro.html": "src/speakers/angelina-fabbro.html",
-        "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html",
-        "public/speakers/mark-boas.html": "src/speakers/mark-boas.html",
-        "public/speakers/estelle-weyl.html": "src/speakers/estelle-weyl.html",
-        "public/speakers/domenic-denicola.html": "src/speakers/domenic-denicola.html",
-        "public/speakers/michal-budzynski.html": "src/speakers/michal-budzynski.html",
-        "public/speakers/oli-evans.html": "src/speakers/oli-evans.html",
-        "public/speakers/tim-park.html": "src/speakers/tim-park.html",
-        "public/speakers/james-halliday.html": "src/speakers/james-halliday.html",
-        "public/speakers/thorsten-lorenz.html": "src/speakers/thorsten-lorenz.html",
-        "public/speakers/andre-cruz.html": "src/speakers/andre-cruz.html",
-        "public/speakers/rod-vagg.html": "src/speakers/rod-vagg.html",
-        "public/speakers/alex-feyerke.html": "src/speakers/alex-feyerke.html",
-        "public/speakers/aral-balkan.html": "src/speakers/aral-balkan.html",
-        "public/speakers/adam-brault.html": "src/speakers/adam-brault.html",
-        "public/speakers/marco-oliveira.html": "src/speakers/marco-oliveira.html",
-        "public/speakers/hannah-wolfe.html": "src/speakers/hannah-wolfe.html",
-        "public/speakers/martin-naumann.html": "src/speakers/martin-naumann.html",
-        "public/speakers/dominic-tarr.html": "src/speakers/dominic-tarr.html",
-        "public/speakers/tancredi-trugenberger.html": "src/speakers/tancredi-trugenberger.html",
-        "public/speakers/caolan-mcmahon.html": "src/speakers/caolan-mcmahon.html"
-  			}
-  		},
+          "public/speakers/mike-brevoort.html": "src/speakers/mike-brevoort.html",
+          "public/speakers/adam-baldwin.html": "src/speakers/adam-baldwin.html",
+          "public/speakers/jonathan-lipps.html": "src/speakers/jonathan-lipps.html",
+          "public/speakers/sara-chipps.html": "src/speakers/sara-chipps.html",
+          "public/speakers/vyacheslav-egorov.html": "src/speakers/vyacheslav-egorov.html",
+          "public/speakers/dustin-diaz.html": "src/speakers/dustin-diaz.html",
+          "public/speakers/brian-leroux.html": "src/speakers/brian-leroux.html",
+          "public/speakers/laura-kalbag.html": "src/speakers/laura-kalbag.html",
+          "public/speakers/shane-hudson.html": "src/speakers/shane-hudson.html",
+          "public/speakers/jed-schmidt.html": "src/speakers/jed-schmidt.html",
+          "public/speakers/andrew-nesbitt.html": "src/speakers/andrew-nesbitt.html",
+          "public/speakers/gilles-ruppert.html": "src/speakers/gilles-ruppert.html",
+          "public/speakers/raquel-velez.html": "src/speakers/raquel-velez.html",
+          "public/speakers/ivan-babrou.html": "src/speakers/ivan-babrou.html",
+          "public/speakers/robert-nyman.html": "src/speakers/robert-nyman.html",
+          "public/speakers/bill-mills.html": "src/speakers/bill-mills.html",
+          "public/speakers/angelina-fabbro.html": "src/speakers/angelina-fabbro.html",
+          "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html",
+          "public/speakers/mark-boas.html": "src/speakers/mark-boas.html",
+          "public/speakers/estelle-weyl.html": "src/speakers/estelle-weyl.html",
+          "public/speakers/domenic-denicola.html": "src/speakers/domenic-denicola.html",
+          "public/speakers/michal-budzynski.html": "src/speakers/michal-budzynski.html",
+          "public/speakers/oli-evans.html": "src/speakers/oli-evans.html",
+          "public/speakers/tim-park.html": "src/speakers/tim-park.html",
+          "public/speakers/james-halliday.html": "src/speakers/james-halliday.html",
+          "public/speakers/thorsten-lorenz.html": "src/speakers/thorsten-lorenz.html",
+          "public/speakers/andre-cruz.html": "src/speakers/andre-cruz.html",
+          "public/speakers/rod-vagg.html": "src/speakers/rod-vagg.html",
+          "public/speakers/alex-feyerke.html": "src/speakers/alex-feyerke.html",
+          "public/speakers/aral-balkan.html": "src/speakers/aral-balkan.html",
+          "public/speakers/adam-brault.html": "src/speakers/adam-brault.html",
+          "public/speakers/marco-oliveira.html": "src/speakers/marco-oliveira.html",
+          "public/speakers/hannah-wolfe.html": "src/speakers/hannah-wolfe.html",
+          "public/speakers/martin-naumann.html": "src/speakers/martin-naumann.html",
+          "public/speakers/dominic-tarr.html": "src/speakers/dominic-tarr.html",
+          "public/speakers/tancredi-trugenberger.html": "src/speakers/tancredi-trugenberger.html",
+          "public/speakers/caolan-mcmahon.html": "src/speakers/caolan-mcmahon.html"
+        }
+      },
 
 
-  		dist: {
-  			options: {
+      dist: {
+       options: {
   				//content: "content.json"
   			},
   			files: {
@@ -105,116 +105,116 @@ module.exports = function(grunt) {
   				"public/register.html": "src/register.html",
   				"public/team-and-contacts.html": "src/team-and-contacts.html",
   				"public/workshops.html": "src/workshops.html",
-    			"public/speakers/index.html": "src/speakers/index.html",
-				"public/speakers/mike-brevoort.html": "src/speakers/mike-brevoort.html",
-				"public/speakers/adam-baldwin.html": "src/speakers/adam-baldwin.html",
-        "public/speakers/jonathan-lipps.html": "src/speakers/jonathan-lipps.html",
-				"public/speakers/sara-chipps.html": "src/speakers/sara-chipps.html",
-				"public/speakers/vyacheslav-egorov.html": "src/speakers/vyacheslav-egorov.html",
-				"public/speakers/dustin-diaz.html": "src/speakers/dustin-diaz.html",
-				"public/speakers/brian-leroux.html": "src/speakers/brian-leroux.html",
-        "public/speakers/laura-kalbag.html": "src/speakers/laura-kalbag.html",
-        "public/speakers/shane-hudson.html": "src/speakers/shane-hudson.html",
-        "public/speakers/jed-schmidt.html": "src/speakers/jed-schmidt.html",
-        "public/speakers/andrew-nesbitt.html": "src/speakers/andrew-nesbitt.html",
-        "public/speakers/gilles-ruppert.html": "src/speakers/gilles-ruppert.html",
-        "public/speakers/raquel-velez.html": "src/speakers/raquel-velez.html",
-        "public/speakers/ivan-babrou.html": "src/speakers/ivan-babrou.html",
-        "public/speakers/robert-nyman.html": "src/speakers/robert-nyman.html",
-        "public/speakers/bill-mills.html": "src/speakers/bill-mills.html",
-        "public/speakers/angelina-fabbro.html": "src/speakers/angelina-fabbro.html",
-        "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html",
-        "public/speakers/mark-boas.html": "src/speakers/mark-boas.html",
-        "public/speakers/estelle-weyl.html": "src/speakers/estelle-weyl.html",
-        "public/speakers/domenic-denicola.html": "src/speakers/domenic-denicola.html",
-        "public/speakers/michal-budzynski.html": "src/speakers/michal-budzynski.html",
-        "public/speakers/oli-evans.html": "src/speakers/oli-evans.html",
-        "public/speakers/tim-park.html": "src/speakers/tim-park.html",
-        "public/speakers/james-halliday.html": "src/speakers/james-halliday.html",
-        "public/speakers/thorsten-lorenz.html": "src/speakers/thorsten-lorenz.html",
-        "public/speakers/andre-cruz.html": "src/speakers/andre-cruz.html",
-        "public/speakers/rod-vagg.html": "src/speakers/rod-vagg.html",
-        "public/speakers/alex-feyerke.html": "src/speakers/alex-feyerke.html",
-        "public/speakers/aral-balkan.html": "src/speakers/aral-balkan.html",
-        "public/speakers/adam-brault.html": "src/speakers/adam-brault.html",
-        "public/speakers/marco-oliveira.html": "src/speakers/marco-oliveira.html",
-        "public/speakers/hannah-wolfe.html": "src/speakers/hannah-wolfe.html",
-        "public/speakers/martin-naumann.html": "src/speakers/martin-naumann.html",
-        "public/speakers/dominic-tarr.html": "src/speakers/dominic-tarr.html",
-        "public/speakers/tancredi-trugenberger.html": "src/speakers/tancredi-trugenberger.html",
-        "public/speakers/caolan-mcmahon.html": "src/speakers/caolan-mcmahon.html"
-			}
-  		}
-  	},
+         "public/speakers/index.html": "src/speakers/index.html",
+         "public/speakers/mike-brevoort.html": "src/speakers/mike-brevoort.html",
+         "public/speakers/adam-baldwin.html": "src/speakers/adam-baldwin.html",
+         "public/speakers/jonathan-lipps.html": "src/speakers/jonathan-lipps.html",
+         "public/speakers/sara-chipps.html": "src/speakers/sara-chipps.html",
+         "public/speakers/vyacheslav-egorov.html": "src/speakers/vyacheslav-egorov.html",
+         "public/speakers/dustin-diaz.html": "src/speakers/dustin-diaz.html",
+         "public/speakers/brian-leroux.html": "src/speakers/brian-leroux.html",
+         "public/speakers/laura-kalbag.html": "src/speakers/laura-kalbag.html",
+         "public/speakers/shane-hudson.html": "src/speakers/shane-hudson.html",
+         "public/speakers/jed-schmidt.html": "src/speakers/jed-schmidt.html",
+         "public/speakers/andrew-nesbitt.html": "src/speakers/andrew-nesbitt.html",
+         "public/speakers/gilles-ruppert.html": "src/speakers/gilles-ruppert.html",
+         "public/speakers/raquel-velez.html": "src/speakers/raquel-velez.html",
+         "public/speakers/ivan-babrou.html": "src/speakers/ivan-babrou.html",
+         "public/speakers/robert-nyman.html": "src/speakers/robert-nyman.html",
+         "public/speakers/bill-mills.html": "src/speakers/bill-mills.html",
+         "public/speakers/angelina-fabbro.html": "src/speakers/angelina-fabbro.html",
+         "public/speakers/charlie-crane.html": "src/speakers/charlie-crane.html",
+         "public/speakers/mark-boas.html": "src/speakers/mark-boas.html",
+         "public/speakers/estelle-weyl.html": "src/speakers/estelle-weyl.html",
+         "public/speakers/domenic-denicola.html": "src/speakers/domenic-denicola.html",
+         "public/speakers/michal-budzynski.html": "src/speakers/michal-budzynski.html",
+         "public/speakers/oli-evans.html": "src/speakers/oli-evans.html",
+         "public/speakers/tim-park.html": "src/speakers/tim-park.html",
+         "public/speakers/james-halliday.html": "src/speakers/james-halliday.html",
+         "public/speakers/thorsten-lorenz.html": "src/speakers/thorsten-lorenz.html",
+         "public/speakers/andre-cruz.html": "src/speakers/andre-cruz.html",
+         "public/speakers/rod-vagg.html": "src/speakers/rod-vagg.html",
+         "public/speakers/alex-feyerke.html": "src/speakers/alex-feyerke.html",
+         "public/speakers/aral-balkan.html": "src/speakers/aral-balkan.html",
+         "public/speakers/adam-brault.html": "src/speakers/adam-brault.html",
+         "public/speakers/marco-oliveira.html": "src/speakers/marco-oliveira.html",
+         "public/speakers/hannah-wolfe.html": "src/speakers/hannah-wolfe.html",
+         "public/speakers/martin-naumann.html": "src/speakers/martin-naumann.html",
+         "public/speakers/dominic-tarr.html": "src/speakers/dominic-tarr.html",
+         "public/speakers/tancredi-trugenberger.html": "src/speakers/tancredi-trugenberger.html",
+         "public/speakers/caolan-mcmahon.html": "src/speakers/caolan-mcmahon.html"
+       }
+     }
+   },
 
-  	/* compass */
+   /* compass */
 
-  	compass: {
-  		compile: {
-  			options: {
-  	  			force: true,
+   compass: {
+    compile: {
+     options: {
+      force: true,
 
-  			require: ['zurb-foundation'],
-	  		sassDir: 'src/sass',
-	  		cssDir: 'public/css',
-	  		environment: 'production'
-	  		}
-  		},
-  		build: {
-  			options: {
-   			force: true,
-  			require: ['zurb-foundation'],
-	  		sassDir: 'src/sass',
-	  		cssDir: 'public/css',
-	  		environment: 'development'
-	  		}
-  		}
-  	},
+      require: ['zurb-foundation'],
+      sassDir: 'src/sass',
+      cssDir: 'public/css',
+      environment: 'production'
+    }
+  },
+  build: {
+   options: {
+    force: true,
+    require: ['zurb-foundation'],
+    sassDir: 'src/sass',
+    cssDir: 'public/css',
+    environment: 'development'
+  }
+}
+},
 
-  	/* concat */
+/* concat */
 
-  	concat: {
-  		dist: {
-  			src: ['assets/src/**/*.js'],
-  			dest: 'public/js/app.js'
-  		}
-  	},
+concat: {
+  dist: {
+   src: ['assets/src/**/*.js'],
+   dest: 'public/js/app.js'
+ }
+},
 
-  	/* uglify */
+/* uglify */
 
-  	uglify: {
-  		dist: {
-  			src: ['src/js/main.js'],
-  			dest: 'public/js/main.js'
-  		}
-  	},
-
-
+uglify: {
+  dist: {
+   src: ['src/js/main.js'],
+   dest: 'public/js/main.js'
+ }
+},
 
 
-  	/* watch */
 
 
-  	watch: {
+/* watch */
 
-  		compass: {
-  			files: ['src/sass/*.sass', 'src/sass/*.scss'],
-  			tasks: 'compass:compile',
-  			options: {
-	  			force: true,
-  				require: ['zurb-foundation'],
-  				sassDir: 'src/sass',
-  				cssDir: 'public/css',
 
-  			}
-  		},
+watch: {
 
-  		bake: {
-  			files: [ "src/**/*" ],
-  			tasks: ["bake:compile","compass:compile"]
-  		}
-  	}
-  });
+  compass: {
+   files: ['src/sass/*.sass', 'src/sass/*.scss'],
+   tasks: 'compass:compile',
+   options: {
+    force: true,
+    require: ['zurb-foundation'],
+    sassDir: 'src/sass',
+    cssDir: 'public/css',
+
+  }
+},
+
+bake: {
+ files: [ "src/**/*" ],
+ tasks: ["bake:compile","compass:compile"]
+}
+}
+});
 
 
 
